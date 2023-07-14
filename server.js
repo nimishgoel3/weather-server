f47e520360141f351cc6de0d30949a95291c2101
Shubham <nss.shubham143@gmail.com>
2023-07-14 11:47:04 +0530

    fix: db issue

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 // mongoose.connect('mongodb://localhost:27017/weatherApp', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://dbuser:Abc%401234@cluster0.vlgbwew.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+  .connect(
+    `mongodb+srv://nimishgoel3:dwfzvTezPIEifqmS@cluster0.c3epvqs.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("connected to database...");
+  })
+  .catch(() => {
+    console.log("failed connected to database");
+  });
 // process.env.MONGO_CONNECTION_STRING
 
 const CitySchema = new mongoose.Schema({
@@ -15,13 +28,15 @@ const CitySchema = new mongoose.Schema({
   searches: Number,
 });
 
-const City = mongoose.model('City', CitySchema);
-const WEATHER_API_KEY = '3ee9d7c9078b1d80b7a774b6482e680c'
+const City = mongoose.model("City", CitySchema);
+const WEATHER_API_KEY = "3ee9d7c9078b1d80b7a774b6482e680c";
 app.use(express.json());
 
-app.get('/api/weather/:city', async (req, res) => {
+app.get("/api/weather/:city", async (req, res) => {
   try {
-    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${WEATHER_API_KEY}`);
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${WEATHER_API_KEY}`
+    );
     let city = await City.findOne({ name: req.params.city });
 
     if (city) {
@@ -34,8 +49,8 @@ app.get('/api/weather/:city', async (req, res) => {
 
     res.json(data);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
-app.listen(5001, () => console.log('Server started on port 5001'));
+app.listen(5001, () => console.log("Server started on port 5001"));
